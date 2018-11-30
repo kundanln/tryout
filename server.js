e8f5055368c2097760bd5a2b77302517523ff0bb
kundanln <kundannarkhede93@gmail.com>
2018-11-30 04:28:09 -0800

    first commit

--- server.js
+++ server.js
@@ -5,14 +5,14 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/CMS-v02'));
+app.use(express.static('./dist/try'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname,'./dist/CMS-v02/index.html'));
+res.sendFile(path.join(__dirname,'./dist/try/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 5000, ()=>{
+app.listen(process.env.PORT || 8080, ()=>{
 	console.log('server started');
 });
